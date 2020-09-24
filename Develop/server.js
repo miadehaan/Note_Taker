068db6e3065b6cea8e68f098a8f877478309dd61
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-09-23 22:37:25 -0700

    server added using node/express on PORT 3000

---
+++ Develop/server.js
@@ -0,0 +1,30 @@
+// Setup dependencies
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+// Initialize port
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../Develop/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+});
+
+// Listener - "start" the server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
