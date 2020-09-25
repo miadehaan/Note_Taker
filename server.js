5ba26ae92c9c151a111d816e8d07d5c7960b0b39
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-09-24 18:51:19 -0700

    files re-arranged

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var express = require("express");
 var path = require("path");
 
+// add 'uuid' library to create new, unique id's for each task
+// var uuid = require("uuid");
+
 var app = express();
 
 // Initialize port
